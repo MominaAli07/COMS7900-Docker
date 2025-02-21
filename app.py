545139d36d6bfe9e4e6c8ad85eb84cf1dee84dc9
Momina Liaqat Ali <153584391+MominaAli07@users.noreply.github.com>
2025-02-21 13:50:10 -0600

    app.py

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def calculator():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
 
